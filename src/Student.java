5c45ec1fad6e0bfd9811444a3f7f222925b6a314
Aswathi AjithKumar <aswathiajith4@gmail.com>
2025-10-23 23:02:45 +0530

    added student.java

---
+++ src/Student.java
@@ -0,0 +1,27 @@
+public class Student {
+    private String id;
+    private String name;
+    private int age;
+    private String grade;
+
+    public Student(String id, String name, int age, String grade) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+
+    public String getId() { return id; }
+    public String getName() { return name; }
+    public int getAge() { return age; }
+    public String getGrade() { return grade; }
+
+    public void setName(String name) { this.name = name; }
+    public void setAge(int age) { this.age = age; }
+    public void setGrade(String grade) { this.grade = grade; }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + ", Name: " + name + ", Age: " + age + ", Grade: " + grade;
+    }
+}
